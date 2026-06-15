ea5e7cda345f384c8141d5bbd141ad3a00b79168
jishana <189jishana@gmail.com>
2026-06-15 17:24:53 +0530

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser automatically if environment variables are set
+python manage.py createsuperuser --noinput || true
